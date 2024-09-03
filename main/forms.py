c7e540513b2c0e6d7758669ddcfd14e8f19b85e6
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-09-03 13:43:15 +0100

    :zap: fo :chart_with_upwards_trend: rms :twisted_rightwards_arrows:

---
+++ main/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class SignupForm(forms.Form):
+    email = forms.EmailField()
+    last_name = forms.CharField()
+    first_name = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class VerificatonForm(forms.Form):
+    theFile = forms.FileField()
+    bankName = forms.CharField()
+    accountNum = forms.CharField()
+    accountName = forms.CharField()
