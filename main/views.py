8748fa0ff0388eecf9d1b5e613a8f8eabca3aa25
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-09-04 14:13:21 +0100

    :chart_with_upwards_trend: load th :globe_with_meridians: e tasks :clipboard:

--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from datetime import datetime
+from django.utils import timezone
+import json
+from .models import User, UserTasks, Documents, Token, Tasks
 
-# Create your views here.
+
+def load(request):
+    json_file_path = 'tasks.json'
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+        for item in data:
+            Tasks.objects.create(title=item['title'], amount=0.7, description=item['description'])
+    return JsonResponse({'success': True, 'message': 'Tasks loaded successfully'}, status=200)
+
+def home(request):
+    return render(request, 'home.html')
+
