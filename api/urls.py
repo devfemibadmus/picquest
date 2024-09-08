0242e1110b042b0fa18e16e78ef162d9a2406f5c
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-09-09 00:37:43 +0100

    :goal_net: api  :bug: urls :construction:

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="Home"),
-    path('signup/', views.signup, name="Signup"),
     # path('load/', views.load, name="Load")
+    path('login/', views.signin, name="signin"),
+    path('signup/', views.signup, name="Signup"),
+    path('tasks/', views.tasks, name="tasks"),
+    path('status/', views.status, name="status"),
+    path('submit/', views.submit, name="submit"),
+    path('user/', views.UserView.getUserData, name="getuser"),
+    path('verification/', views.verification, name="verification"),
 ]
