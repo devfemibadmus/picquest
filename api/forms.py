fa1327ca01447d8147c83234386f0a8a93747633
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-09-08 02:27:55 +0100

    :white_check_mark: signi :gem: n form :recycle:

--- api/forms.py
+++ api/forms.py
@@ -5,6 +5,9 @@ class SignupForm(forms.Form):
     last_name = forms.CharField()
     first_name = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
 class VerificatonForm(forms.Form):
     theFile = forms.FileField()
