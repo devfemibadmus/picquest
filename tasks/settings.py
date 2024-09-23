3df29d6090ef06fd9567821aa113702fb9f52347
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-09-23 03:31:47 +0100

    :chart_with_upwards_trend: sta :bug: tics :twisted_rightwards_arrows:

--- tasks/settings.py
+++ tasks/settings.py
@@ -146,7 +146,14 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'statics'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'picquest.online'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'picquest@picquest.online'
+EMAIL_HOST_PASSWORD = 'Hellopeter1$'
+DEFAULT_FROM_EMAIL = 'Pic Quest AI <picquest@picquest.online>'
+CUSTOMER_SUPPORT_EMAIL = 'support@picquest.online'
+UPPERCASED_BANK_NAME = 'Pic Quest AI'
